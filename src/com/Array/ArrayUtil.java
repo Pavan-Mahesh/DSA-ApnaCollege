3ca85d47e8e5145ad7f43eed56ba6c2f4211db43
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2024-09-06 19:47:03 +0530

    starting dsa with apna college

--- src/com/Array/ArrayUtil.java
+++
@@ -1,9 +0,0 @@
-package com.Array;
-
-class ArrayUtil {
-    public static void print (int[] arr) {
-        for (int i : arr)
-            System.out.print(i + " ");
-        System.out.println();
-    }
-}
---
+++ src/com/HomeWork/Day4_Pattern/Butterfly.java
@@ -0,0 +1,33 @@
+package com.HomeWork.Day4_Pattern;
+
+public class Butterfly {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i + 1; j++)
+                System.out.print("* ");
+
+            for(int j = 0; j < 2 * (n - i - 1); j++)
+                System.out.print("  ");
+
+            for(int j = 0; j < i + 1; j++)
+                System.out.print("* ");
+
+            System.out.println();
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i; j++)
+                System.out.print("* ");
+
+            for(int j = 0; j < 2 * i; j++)
+                System.out.print("  ");
+
+            for(int j = 0; j < n - i; j++)
+                System.out.print("* ");
+
+            System.out.println();
+        }
+    }
+}
